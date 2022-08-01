a79085e662287fb665da5deda3122203c603e85d
cecilia <ceciliarodriguesdes@gmail.com>
2022-07-31 22:05:38 -0300

    Testing JS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let displayCal = document.querySelectorAll('.display');
+function insert(num) {
+
+    let n = document.querySelector('.display').innerHTML;
+    document.querySelector('.display').innerHTML = n + num;
+}
+function clean() {
+    document.querySelector('.display').innerHTML = '';
+}
+function back(){
+    let display = document.querySelector('.display').innerHTML;
+    document.querySelector('.display').innerHTML = display.substring(0, display.length -1);
+}
+function result(){
+    let display = document.querySelector('.display').innerHTML;
+    if(display) {
+        document.querySelector('.display').innerHTML = eval(display);
+    }
+    else {
+        document.querySelector('.display').innerHTML = '0';
+    }
+}
